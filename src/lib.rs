b532041c12021d1051c66a29f39a3712e3743100
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-11 10:47:22 +0900

    feat: add verify_password

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use argon2::{password_hash::Salt, Argon2, PasswordHasher};
+use argon2::{password_hash::Salt, Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
+use password_hash::Error;
 use wasm_bindgen::{prelude::wasm_bindgen, JsError};
 
 #[wasm_bindgen]
@@ -12,4 +13,16 @@ pub fn hash_password_simple(password: String, salt_b64: String) -> Result<String
         .map_err(|e| JsError::new(&format!("[Argon2::hash_password] {}", e)))?;
 
     Ok(password_hash.to_string())
+}
+
+#[wasm_bindgen]
+pub fn verify_password(password: String, password_hash: String) -> Result<bool, JsError> {
+    let parsed_password_hash = PasswordHash::new(&password_hash)
+        .map_err(|e| JsError::new(&format!("[PasswordHash::new] {}", e)))?;
+    let result = Argon2::default().verify_password(password.as_bytes(), &parsed_password_hash);
+    match result {
+        Ok(()) => Ok(true),
+        Err(Error::Password) => Ok(false),
+        Err(e) => Err(JsError::new(&format!("[Argon2::verify_password] {}", e))),
+    }
 }
--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,5 +1,9 @@
 import { beforeAll, describe, expect, it } from "vitest";
-import { initSync, hash_password_simple } from "../pkg/index.js";
+import {
+  initSync,
+  hash_password_simple,
+  verify_password,
+} from "../pkg/index.js";
 import fs from "node:fs";
 
 beforeAll(async () => {
@@ -27,6 +31,26 @@ describe(hash_password_simple, () => {
       hash_password_simple("pepper", salt)
     ).toThrowErrorMatchingInlineSnapshot(
       "\"[Salt::from_b64] salt invalid: contains invalid character: '='\""
+    );
+  });
+});
+
+describe("example", () => {
+  it("basic", async () => {
+    const salt = btoa("salt".repeat(3));
+    expect(salt).toMatchInlineSnapshot('"c2FsdHNhbHRzYWx0"');
+
+    const password = "pepper";
+    const password_hash = hash_password_simple(password, salt);
+    expect(password_hash).toMatchInlineSnapshot(
+      '"$argon2id$v=19$m=19456,t=2,p=1$c2FsdHNhbHRzYWx0$In0aM6WcpzTnOC0J2iJ8Us5IfVCteAX3tEfKF2tNlIQ"'
+    );
+
+    expect(verify_password(password, password_hash)).toMatchInlineSnapshot(
+      "true"
+    );
+    expect(verify_password("wrong", password_hash)).toMatchInlineSnapshot(
+      "false"
     );
   });
 });
