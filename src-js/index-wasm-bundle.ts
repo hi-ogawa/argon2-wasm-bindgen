c421df59942a7eed711a5c4f05fe69a3b169dbc9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-11 13:22:20 +0900

    chore: tweak export

--- src-js/index-wasm-bundle.ts
+++ src-js/index-wasm-bundle.ts
@@ -1,11 +1,10 @@
-import * as wasmBindgenModule from "../pkg/index";
-
-export { wasmBindgenModule };
+import initWasmBindGen from "../pkg/index";
+export * from "../pkg/index";
 
 export async function initializeBundle() {
   // inline ~100KB binary of base64 (cf. tsup.config.ts)
   const imported = await import("../pkg/index_bg.wasm");
   const wasmSource = imported.default as unknown as Uint8Array;
   const wasmModule = await WebAssembly.compile(wasmSource);
-  await wasmBindgenModule.default(wasmModule);
+  await initWasmBindGen(wasmModule);
 }
--- test/bundle.test.ts
+++ test/bundle.test.ts
@@ -1,5 +1,9 @@
 import { beforeAll, describe, expect, it } from "vitest";
-import { wasmBindgenModule, initializeBundle } from "../dist/index-wasm-bundle";
+import {
+  hash_password,
+  verify_password,
+  initializeBundle,
+} from "../dist/index-wasm-bundle";
 
 beforeAll(async () => {
   await initializeBundle();
@@ -9,16 +13,16 @@ describe("bundle", () => {
   it("basic", async () => {
     const password = "password";
     const salt = btoa("salt".repeat(3));
-    const password_hash = wasmBindgenModule.hash_password(password, salt);
+    const password_hash = hash_password(password, salt);
     expect(password_hash).toMatchInlineSnapshot(
       '"$argon2id$v=19$m=19456,t=2,p=1$c2FsdHNhbHRzYWx0$83NhH4a54XrGQTfUWbmBlx7cfoFGhD1nGBk7kNt4L44"'
     );
 
-    expect(
-      wasmBindgenModule.verify_password(password, password_hash)
-    ).toMatchInlineSnapshot("true");
-    expect(
-      wasmBindgenModule.verify_password("wrong", password_hash)
-    ).toMatchInlineSnapshot("false");
+    expect(verify_password(password, password_hash)).toMatchInlineSnapshot(
+      "true"
+    );
+    expect(verify_password("wrong", password_hash)).toMatchInlineSnapshot(
+      "false"
+    );
   });
 });
