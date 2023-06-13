b911b7973cb5e110d739225d8dde9f13f915463a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-13 21:30:48 +0900

    refactor: simplify tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,38 +1,45 @@
-import { defineConfig, Options } from "tsup";
+import { defineConfig } from "tsup";
 import fs from "node:fs";
 import process from "node:process";
 
+// build in two steps so that 2nd step can use the 1st step's output
+
 export default [
   process.env["BUILD_STEP"] == "1" &&
-    defineConfig({
-      entry: ["src-js/bundle.ts", "src-js/comlink.ts"],
-      format: ["esm", "cjs"],
-      dts: true,
-      platform: "neutral",
-      loader: {
-        ".wasm": "binary",
+    defineConfig([
+      {
+        entry: ["src-js/bundle.ts", "src-js/comlink.ts"],
+        format: ["esm", "cjs"],
+        dts: true,
+        platform: "neutral",
+        loader: {
+          ".wasm": "binary",
+        },
       },
-    }),
-  process.env["BUILD_STEP"] == "1" &&
-    defineConfig({
-      entry: ["src-js/comlink-node.ts"],
-      format: ["esm", "cjs"],
-      dts: true,
-      platform: "node",
-      loader: {
-        ".wasm": "binary",
+      {
+        entry: ["src-js/comlink-node.ts"],
+        format: ["esm", "cjs"],
+        dts: true,
+        platform: "node",
+        loader: {
+          ".wasm": "binary",
+        },
       },
-    }),
+    ]),
   process.env["BUILD_STEP"] == "2" &&
-    defineConfig({
-      entry: ["src-js/comlink-node-proxy.ts"],
-      format: ["esm", "cjs"],
-      dts: true,
-      platform: "node",
-      define: {
-        DEFINE_WORKER_CODE: JSON.stringify(
-          fs.readFileSync("./dist/comlink-node.js", "utf-8")
-        ),
+    defineConfig([
+      {
+        entry: ["src-js/comlink-node-proxy.ts"],
+        format: ["esm", "cjs"],
+        dts: true,
+        platform: "node",
+        define: {
+          DEFINE_WORKER_CODE: JSON.stringify(
+            fs.readFileSync("./dist/comlink-node.js", "utf-8")
+          ),
+        },
       },
-    }),
-].filter(Boolean);
+    ]),
+]
+  .filter(Boolean)
+  .flat();
