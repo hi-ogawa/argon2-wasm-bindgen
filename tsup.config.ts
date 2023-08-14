b0893556eb57181749ac196c4cd9345919145cbc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-14 17:30:53 +0900

    chore: remove cjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,56 +5,52 @@ import process from "node:process";
 // build in two steps so that 2nd step can use the 1st step's output
 
 export default [
-  process.env["BUILD_STEP"] == "1" &&
-    defineConfig([
-      {
-        entry: ["src-js/bundle.ts", "src-js/worker-web-main.ts"],
-        format: ["esm", "cjs"],
-        dts: true,
-        splitting: false,
-        platform: "neutral",
-        loader: {
-          ".wasm": "binary",
-        },
+  defineConfig(() => [
+    {
+      entry: ["src-js/bundle.ts", "src-js/worker-web-main.ts"],
+      format: ["esm"],
+      dts: true,
+      splitting: false,
+      platform: "neutral",
+      loader: {
+        ".wasm": "binary",
       },
-      {
-        entry: ["src-js/worker-node-main.ts"],
-        format: ["esm", "cjs"],
-        dts: true,
-        splitting: false,
-        platform: "node",
-        loader: {
-          ".wasm": "binary",
-        },
+    },
+    {
+      entry: ["src-js/worker-node-main.ts"],
+      format: ["esm"],
+      dts: true,
+      splitting: false,
+      platform: "node",
+      loader: {
+        ".wasm": "binary",
       },
-    ]),
-  process.env["BUILD_STEP"] == "2" &&
-    defineConfig([
-      {
-        entry: ["src-js/worker-web.ts"],
-        format: ["esm", "cjs"],
-        dts: true,
-        splitting: false,
-        platform: "neutral",
-        define: {
-          DEFINE_WORKER_CODE: JSON.stringify(
-            fs.readFileSync("./dist/worker-web-main.js", "utf-8")
-          ),
-        },
+    },
+  ]),
+  defineConfig(() => [
+    {
+      entry: ["src-js/worker-web.ts"],
+      format: ["esm"],
+      dts: true,
+      splitting: false,
+      platform: "neutral",
+      define: {
+        DEFINE_WORKER_CODE: JSON.stringify(
+          fs.readFileSync("./dist/worker-web-main.js", "utf-8")
+        ),
       },
-      {
-        entry: ["src-js/worker-node.ts"],
-        format: ["esm", "cjs"],
-        dts: true,
-        splitting: false,
-        platform: "node",
-        define: {
-          DEFINE_WORKER_CODE: JSON.stringify(
-            fs.readFileSync("./dist/worker-node-main.js", "utf-8")
-          ),
-        },
+    },
+    {
+      entry: ["src-js/worker-node.ts"],
+      format: ["esm"],
+      dts: true,
+      splitting: false,
+      platform: "node",
+      define: {
+        DEFINE_WORKER_CODE: JSON.stringify(
+          fs.readFileSync("./dist/worker-node-main.js", "utf-8")
+        ),
       },
-    ]),
-]
-  .filter(Boolean)
-  .flat();
+    },
+  ]),
+][process.env["BUILD_STEP"]!];
