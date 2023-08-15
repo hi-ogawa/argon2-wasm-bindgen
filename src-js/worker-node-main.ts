d2ee813f44e3b87b9f1b7524eb85ec1ea2f52d7a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-15 13:46:51 +0900

    refactor: replace comlink with tiny-rpc

--- src-js/worker-node-main.ts
+++ src-js/worker-node-main.ts
@@ -1,12 +1,31 @@
 import * as argon2 from "./bundle";
-import { expose } from "comlink";
-import comlinkNodeAdapter from "comlink/dist/umd/node-adapter";
 import { parentPort } from "node:worker_threads";
+import { tinyassert } from "@hiogawa/utils";
+import {
+  exposeRpc,
+  messagePortServerAdapter,
+} from "@hiogawa/tiny-rpc/dist/index-v2";
 
 export type Argon2 = typeof argon2;
 
 function main() {
-  expose(argon2, comlinkNodeAdapter(parentPort!));
+  tinyassert(parentPort);
+
+  parentPort.once("message", (ev: unknown) => {
+    tinyassert(
+      ev &&
+        typeof ev === "object" &&
+        "port" in ev &&
+        ev.port instanceof MessagePort
+    );
+
+    exposeRpc({
+      routes: argon2,
+      adapter: messagePortServerAdapter({
+        port: ev.port,
+      }),
+    });
+  });
 }
 
 main();
--- src-js/worker-node.ts
+++ src-js/worker-node.ts
@@ -1,7 +1,10 @@
-import { wrap } from "comlink";
-import comlinkNodeAdapter from "comlink/dist/umd/node-adapter";
 import { Worker } from "node:worker_threads";
 import { Argon2 } from "./worker-node-main";
+import {
+  proxyRpc,
+  messagePortClientAdapter,
+} from "@hiogawa/tiny-rpc/dist/index-v2";
+
 export type { Argon2 };
 
 declare let DEFINE_WORKER_CODE: string;
@@ -11,6 +14,13 @@ export async function initWorker() {
     `data:text/javascript,${encodeURIComponent(DEFINE_WORKER_CODE)}`
   );
   const worker = new Worker(url);
-  const argon2 = wrap<Argon2>(comlinkNodeAdapter(worker));
+
+  // TODO: confirm initial hand-shake?
+  const channel = new MessageChannel();
+  worker.postMessage({ port: channel.port1 }, [channel.port1 as any]);
+
+  const argon2 = proxyRpc<Argon2>({
+    adapter: messagePortClientAdapter({ port: channel.port2 }),
+  });
   return { worker, argon2 };
 }
